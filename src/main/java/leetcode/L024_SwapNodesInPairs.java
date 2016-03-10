9b2aa5c9796e183ba392209b7a5d4c6082e6c9a5
IS908 <1196355049@qq.com>
2016-03-10 22:39:40 +0800

    单链表操作部分。

---
+++ src/main/java/leetcode/L024_SwapNodesInPairs.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p/>
+ * For example,
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * <p/>
+ * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ * <p/>
+ * Created by kevin on 2016/3/10.
+ */
+@Deprecated
+public class L024_SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = head.next, a = head, b = head.next, c = null;
+        while (b != null) {
+            a.next = b.next;
+            b.next = a;
+            if (c != null) c.next = b;
+            if (a.next == null) break;
+            b = a.next.next;
+            c = a;
+            a = a.next;
+        }
+        return newHead;
+    }
+}
--- src/main/java/leetcode/L061_RotateList.java
+++ src/main/java/leetcode/L061_RotateList.java
@@ -11,21 +11,40 @@ package leetcode;
  */
 public class L061_RotateList {
     public ListNode rotateRight(ListNode head, int k) {
-        if (k == 0) return head;
-        ListNode p, q, r, s;
+        if (head == null || k <= 0)
+            return head;
+
+        ListNode tail = head;
+        int len = 1;//
+        while (tail.next != null) {// 计算表长并找到尾结点，使其next域指向第一个节点
+            len++;
+            tail = tail.next;
+        }
+        tail.next = head;//
+        k = k % len;// k可能是比链表长度要大的数字，因此实际旋转的位置就是k%len;
+        ListNode p = head;
+        for (int i = 0; i < len - k - 1; i++)
+            p = p.next; // 找到第n-k个结点
+
+        head = p.next;// 断链
+        p.next = null;
+        return head;
+
+        /*if (k == 0 || head == null) return head;
+        ListNode p, q;
         p = head;
         q = p;
-        r = head;
         while (p.next != null) {
             if (k>0) {
-                q = p;
-                r = q.next;
+                k--;
+            } else {
+                q = q.next;
             }
             p = p.next;
-            k--;
         }
         p.next = head;
+        head = q.next;
         q.next = null;
-        return r;
+        return head;*/
     }
 }
