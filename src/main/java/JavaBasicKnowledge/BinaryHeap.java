6e0b2e3877bb052b7953ff1bea66908f900be18f
IS908 <1196355049@qq.com>
2016-03-17 21:19:58 +0800

    二叉堆的框架搭建！

---
+++ src/main/java/JavaBasicKnowledge/BinaryHeap.java
@@ -0,0 +1,59 @@
+package JavaBasicKnowledge;
+
+/**
+ * 二项堆的数据结构及相关操作类
+ * <p/>
+ * Created by kevin on 16-3-17.
+ */
+public class BinaryHeap<AnyType extends Comparable<? super AnyType>> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int currentSize;
+    private AnyType[] array;
+
+    public BinaryHeap() {
+
+    }
+
+    public BinaryHeap(int capacity) {
+
+    }
+
+    public BinaryHeap(AnyType[] items) {
+
+    }
+
+    public void insert(AnyType x) {
+
+    }
+
+    public AnyType findMin() {
+
+        return null;
+    }
+
+    public AnyType deleteMin() {
+
+        return null;
+    }
+
+    public boolean isEmpty() {
+
+        return false;
+    }
+
+    public void makeEmpty() {
+
+    }
+
+    private void percolateDown(int hole) {
+
+    }
+
+    private void buildHeap() {
+
+    }
+
+    private void enlargeArray(int newSize) {
+
+    }
+}
