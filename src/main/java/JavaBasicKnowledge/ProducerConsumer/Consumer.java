67fa71dd3c5ade72b89d3713316944f8defa7c6d
IS908 <1196355049@qq.com>
2016-03-19 11:03:58 +0800

    生产者消费者问题模拟！

--- src/main/java/JavaBasicKnowledge/ProducerConsumer/Consumer.java
+++ src/main/java/JavaBasicKnowledge/ProducerConsumer/Consumer.java
@@ -1,7 +1,19 @@
 package JavaBasicKnowledge.ProducerConsumer;
 
+import java.util.LinkedList;
+
 /**
  * Created by kevin on 16-3-19.
  */
 public class Consumer extends Thread{
+    public LinkedList<Object> list;
+
+    public Consumer(LinkedList<Object> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
 }
