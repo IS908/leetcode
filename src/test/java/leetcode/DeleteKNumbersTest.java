e20f37acb38462777aa3761766596b6adcec5206
IS908 <1196355049@qq.com>
2016-03-20 00:02:12 +0800

    .gitignore settings.

---
+++ src/test/java/leetcode/DeleteKNumbersTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by kevin on 2016/3/5.
+ */
+public class DeleteKNumbersTest {
+    @Test
+    public void test() {
+        DeleteKNumbers deleteKNumbers = new DeleteKNumbers();
+        deleteKNumbers.solution(123, 2);
+    }
+}
