bd4eef0bc4b04b5ad559a15887af2eebc1fb3939
IS908 <1196355049@qq.com>
2016-03-21 14:29:58 +0800

    二叉树的层次Zigzag遍历！

---
+++ src/main/java/leetcode/L099_RecoverBinarySearchTree.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import leetcode.Utils.TreeNode;
+
+/**
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * <p/>
+ * Recover the tree without changing its structure.
+ * <p/>
+ * Created by kevin on 2016/3/20.
+ */
+public class L099_RecoverBinarySearchTree {
+    public void recoverTree(TreeNode root) {
+
+        return;
+    }
+}
--- src/main/java/leetcode/L103_BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/leetcode/L103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,36 +1,36 @@
 package leetcode;
 
 import leetcode.Utils.TreeNode;
-import sun.reflect.generics.tree.Tree;
 
 import java.util.*;
 
 /**
- * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
  * <p/>
  * For example:
  * Given binary tree {3,9,20,#,#,15,7},
  * 3
- * /    \
- * 9     20
- * / \  /  \
- * # # 15   7
- * return its level order traversal as:
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its zigzag level order traversal as:
  * [
  * [3],
- * [9,20],
+ * [20,9],
  * [15,7]
  * ]
  * confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
  * <p/>
  * Created by kevin on 2016/3/20.
  */
-public class L102_BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+public class L103_BinaryTreeZigzagLevelOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> lists = new ArrayList<>();
         if (root == null) return lists;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        queue.add(root);
+        boolean asc = true;
         while (!queue.isEmpty()) {
             List<Integer> list = new ArrayList<>();
             for (int i = queue.size(); i > 0; i--) {
@@ -39,6 +39,14 @@ public class L102_BinaryTreeLevelOrderTraversal {
                 if (node.right != null) queue.offer(node.right);
                 list.add(node.val);
             }
+            if (!asc) {
+                int size = list.size();
+                for (int i = 0; i < size; i++) {
+                    int tmp = list.remove(size - 1);
+                    list.add(i, tmp);
+                }
+            }
+            asc = !asc;
             lists.add(list);
         }
         return lists;
