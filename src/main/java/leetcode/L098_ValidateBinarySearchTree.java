6dc5a264a3e71016f6df3bb924175637d7243502
IS908 <1196355049@qq.com>
2016-04-01 22:48:14 +0800

    Validate Binary Search Tree

--- src/main/java/leetcode/L098_ValidateBinarySearchTree.java
+++ src/main/java/leetcode/L098_ValidateBinarySearchTree.java
@@ -15,7 +15,27 @@ import leetcode.Utils.TreeNode;
  */
 public class L098_ValidateBinarySearchTree {
     public boolean isValidBST(TreeNode root) {
-
-        return false;
+        if (root == null) return true;
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        return root.val > min && root.val < max &&
+                isValidBST(root.left, min, root.val) &&
+                isValidBST(root.right, root.val, max);
+    }
+
+    /*public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
+        return isValidBST(root.left, min, root.val) &&
+                isValidBST(root.right, root.val, max);
+    }*/
 }
