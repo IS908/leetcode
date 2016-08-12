0abf7040623c5d6db6d5482ebe42c83e732f51f0
ubuntuck <1196355049@qq.com>
2016-08-12 21:30:35 +0800

    Remove Duplicates from Sorted List

--- src/main/java/leetcode/L082_RemoveDuplicatesFromSortedListII.java
+++ src/main/java/leetcode/L082_RemoveDuplicatesFromSortedListII.java
@@ -2,6 +2,8 @@ package leetcode;
 
 import leetcode.Utils.ListNode;
 
+import java.util.HashSet;
+
 /**
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
  * <p/>
@@ -11,21 +13,66 @@ import leetcode.Utils.ListNode;
  * <p/>
  * Created by kevin on 2016/3/8.
  */
+@Deprecated
 public class L082_RemoveDuplicatesFromSortedListII {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy = new ListNode(0), p = dummy;
-        while(head != null) {
-            int val = head.val;
-            if (head.next != null && head.next.val == val) {// if we have duplicates in pair of values
-                head = head.next; //get on last duplicate in pair of values
-                while(head != null && head.val == val) head = head.next; // iteratively we get the next value
+    // TODO: 16-8-12 比较巧妙，建议多推敲下
+    // 参照讨论区的解决方案
+    public ListNode deleteDuplicates1(ListNode head) {
+        ListNode newHead = new ListNode(0);
+        ListNode cur = newHead;
+        ListNode next = head;
+        ListNode following = null;
+        while (next != null) {
+            following = next.next;
+            if (following == null || next.val != following.val) {
+                cur.next = next;
+                cur = cur.next;
+                cur.next = null;
             } else {
-                p.next = head; // head points to unique value
-                p = p.next;
-                head = head.next;
+                while (following != null && following.val == next.val) {
+                    following = following.next;
+                }
+            }
+            next = following;
+        }
+        return newHead.next;
+    }
+
+    // 用 HashSet 比较低效
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        HashSet<Integer> set = new HashSet<>();
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) set.add(cur.val);
+            cur = cur.next;
+        }
+
+        while (head != null && set.contains(head.val)) {
+            head = head.next;
+        }
+
+        if (head == null || head.next == null) return head;
+
+        cur = head;
+        ListNode next = cur.next;
+
+        while (next != null) {
+            boolean flag = set.contains(next.val);
+            while (flag && next != null && next.next != null) {
+                next = next.next;
+                flag = set.contains(next.val);
+            }
+            if (flag) {
+                cur.next = null; // next == null
+                break;
+            } else {
+                cur.next = next;
+                cur = cur.next;
+                next = next.next;
             }
         }
-        p.next = null; //cut off the chain of nodes within original linked list
-        return dummy.next;
+
+        return head;
     }
 }
