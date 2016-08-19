98a33eb55d7b5b742671b8b2c589f50f82b0b8ea
ubuntuck <1196355049@qq.com>
2016-08-19 19:15:38 +0800

    3SumClosest

--- src/main/java/leetcode/L016_3SumClosest.java
+++ src/main/java/leetcode/L016_3SumClosest.java
@@ -11,17 +11,34 @@ import java.util.Arrays;
 public class L016_3SumClosest {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int result = 0, tmp;
+        int result = nums[0] + nums[1] + nums[2], tmp;
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
+            for (int j = i + 1, k = nums.length - 1; j < k; ) {
+                tmp = nums[i] + nums[j] + nums[k];
+                if (Math.abs(tmp - target) < Math.abs(result - target)) {
+                    result = tmp;
+                }
+
+                if (tmp > target) k--;
+                else if (tmp < target) j++;
+                else return target;
+            }
+            /*for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     tmp = nums[i] + nums[j] + nums[k];
                     if (Math.abs(tmp - target) < Math.abs(result - target)) {
                         result = tmp;
                     }
                 }
-            }
+            }*/
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-3,-2,-5,3,-4};
+        L016_3SumClosest sumClosest = new L016_3SumClosest();
+        int res = sumClosest.threeSumClosest(nums, -1);
+        System.out.println(res);
     }
 }
