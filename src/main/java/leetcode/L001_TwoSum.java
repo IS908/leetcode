95c34406152e36f9ec9fe43528fb4d28b9c41649
ubuntuck <1196355049@qq.com>
2016-08-19 17:34:07 +0800

    TwoSum

--- src/main/java/leetcode/L001_TwoSum.java
+++ src/main/java/leetcode/L001_TwoSum.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -12,21 +13,20 @@ import java.util.HashMap;
 public class L001_TwoSum {
     public int[] twoSum(int[] nums, int target) {
         if (nums == null || nums.length == 0) return null;
-        int[] res = null;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) map.put(nums[i], i);
+
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int tmp = target - nums[i];
-            if (map.get(tmp) != null && map.get(tmp) != i) {
-                int gap = map.get(tmp);
-                int higher = i > gap ? i : gap;
-                int lower = i < gap ? i : gap;
-                res = new int[]{lower, higher};
+            int gap = target - nums[i];
+            Integer index = map.get(gap);
+            if (index != null && index != i) {
+                res[0] = Math.min(i, index);
+                res[1] = Math.max(i, index);
                 break;
             }
         }
+
         return res;
     }
 }
