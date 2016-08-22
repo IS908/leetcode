2494a2e2c8186189a9a24dbadb176ab3b6f49d9d
ubuntuck <1196355049@qq.com>
2016-08-22 22:54:51 +0800

    ValidSudoku

--- src/main/java/leetcode/L036_ValidSudoku.java
+++ src/main/java/leetcode/L036_ValidSudoku.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
  * <p/>
@@ -7,10 +10,42 @@ package leetcode;
  * <p/>
  * Created by kevin on 2016/3/6.
  */
-@Deprecated
 public class L036_ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
+        Set<Character> set = new HashSet<>();
+        // 检查行
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                if (set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+            set.clear();
+        }
+
+        // 检查列
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+                if (set.contains(board[j][i])) return false;
+                set.add(board[j][i]);
+            }
+            set.clear();
+        }
+
+        // TODO: 16-8-22  检查小的九宫格
+        for (int k = 0; k < 9; k++) {
+            for (int i = k / 3 * 3; i < k / 3 * 3 + 3; i++) {
+                for (int j = (k % 3) * 3; j < (k % 3) * 3 + 3; j++) {
+                    if (board[i][j] == '.') continue;
+                    if (set.contains(board[i][j])) return false;
+                    set.add(board[i][j]);
+                }
+            }
+            set.clear();
+        }
 
         return true;
     }
+
 }
