125a8d3c07b4d295443e9afd12636816e46aea47
ubuntuck <1196355049@qq.com>
2016-08-22 22:59:25 +0800

    SearchInRotatedSortedArray

--- src/main/java/leetcode/L033_SearchInRotatedSortedArray.java
+++ src/main/java/leetcode/L033_SearchInRotatedSortedArray.java
@@ -2,29 +2,25 @@ package leetcode;
 
 /**
  * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
- * <p/>
  * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
- * <p/>
  * You are given a target value to search. If found in the array return its index, otherwise return -1.
- * <p/>
  * You may assume no duplicate exists in the array.
  * <p/>
  * Created by kevin on 2016/3/3.
  */
 public class L033_SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
         int first = 0, last = nums.length, mid;
         while (first != last) {
             mid = (first + last) / 2;
             if (nums[mid] == target) return mid;
-            if (nums[first] <= nums[mid]) {
+            if (nums[first] <= nums[mid]) { // 突变点在 mid 右边
                 if (nums[first] <= target && target <= nums[mid]) {
                     last = mid;
-                } else {
-                    first = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[last - 1]) {       //*******************
+                } else first = mid + 1;
+            } else {                        // 突变点在 mid 左边
+                if (nums[mid] < target && target <= nums[last - 1]) {
                     first = mid + 1;
                 } else last = mid;
             }
