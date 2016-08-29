7cdc68682676c1b48a6e76fdd08a64270bc9fca3
ubuntuck <1196355049@qq.com>
2016-08-29 20:48:37 +0800

    Candy

--- src/main/java/leetcode/L135_Candy.java
+++ src/main/java/leetcode/L135_Candy.java
@@ -11,26 +11,23 @@ package leetcode;
  * <p/>
  * Created by kevin on 2016/3/7.
  */
-//@Deprecated
 public class L135_Candy {
     public int candy(int[] ratings) {
-        if (ratings == null || ratings.length == 0)    return 0;
-        if (ratings.length == 1)    return 1;
-        int total = 1, len=1, cur = 1, beforeDec = cur;
+        if (ratings == null || ratings.length == 0) return 0;
+        if (ratings.length == 1) return 1;
+        int total = 1, len = 1, cur = 1, beforeDec = cur;
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] < ratings[i - 1]) {
                 total += len;
-                if (beforeDec <= len) {
-                    total++;
-                }
+                if (beforeDec <= len) total++;
                 len++;
                 cur = 1;
             } else {
-                if (ratings[i] == ratings[i - 1]){
+                if (ratings[i] == ratings[i - 1]) {
                     cur = 1;
                     total++;
                 } else {
-                    cur ++;
+                    cur++;
                     total += cur;
                 }
                 len = 1;
@@ -38,5 +35,26 @@ public class L135_Candy {
             }
         }
         return total;
+    }
+
+    public int candy02(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+        if (ratings.length == 1) return 1;
+        int[] cans = new int[ratings.length];
+        cans[0] = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) cans[i] = cans[i - 1] + 1;
+            else cans[i] = 1;
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && cans[i] <= cans[i + 1]) cans[i] = cans[i + 1] + 1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < ratings.length; i++) {
+            sum += cans[i];
+        }
+        return sum;
     }
 }
