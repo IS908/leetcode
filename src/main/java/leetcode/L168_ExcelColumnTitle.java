fd13c207ae4024af5d556954be0281cde6cbefae
ubuntuck <1196355049@qq.com>
2016-09-08 23:05:19 +0800

    Excel Column Title

--- src/main/java/leetcode/L168_ExcelColumnTitle.java
+++ src/main/java/leetcode/L168_ExcelColumnTitle.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
  * <p/>
@@ -19,7 +21,29 @@ package leetcode;
  */
 public class L168_ExcelColumnTitle {
     public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        convert(n - 1, sb);
+        return sb.toString();
+    }
 
-        return null;
+    public void convert(int n, StringBuilder sb) {
+        if (n > 25) convert(n / 26 - 1, sb);
+        sb.append((char) ('A' + (n % 26)));
+    }
+
+    public String convertToTitle02(int n) {
+        Stack<Character> stack = new Stack<>();
+        n--;
+        while (n > 25) {
+            stack.push((char) ('A' + (n % 26)));
+            n = n / 26 - 1;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append((char) ('A' + n));
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
     }
 }
