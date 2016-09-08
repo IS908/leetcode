e1c9875ea60edd0ac308fac66b7a7b92caeca318
ubuntuck <1196355049@qq.com>
2016-09-08 11:02:24 +0800

    Convert SortedList To Binary Search Tree

--- src/main/java/leetcode/L109_ConvertSortedListToBinarySearchTree.java
+++ src/main/java/leetcode/L109_ConvertSortedListToBinarySearchTree.java
@@ -3,9 +3,6 @@ package leetcode;
 import leetcode.Utils.ListNode;
 import leetcode.Utils.TreeNode;
 
-import java.lang.reflect.Array;
-import java.util.*;
-
 /**
  * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
  * <p/>
@@ -13,17 +10,92 @@ import java.util.*;
  */
 public class L109_ConvertSortedListToBinarySearchTree {
     public TreeNode sortedListToBST(ListNode head) {
+        int len = listLength(head);
+//        TreeNode root = sortedListToBST(head, 0, len - 1);
+        TreeNode[] nodes = listToArray(head);
+        TreeNode root = sortedListToBST(nodes, 0, len - 1);
+        return root;
+    }
+
+    /**
+     * 自底向上创建树
+     *
+     * @param node
+     * @param start
+     * @param end
+     * @return
+     */
+    private TreeNode sortedListToBST(TreeNode[] node, int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode leftChild = sortedListToBST(node, start, mid - 1);
+        TreeNode parent = node[mid];
+        parent.left = leftChild;
+        TreeNode rightChild = sortedListToBST(node, mid + 1, end);
+        parent.right = rightChild;
+        return parent;
+    }
+
+    /**
+     * 将链表转化为数组
+     *
+     * @param list
+     * @return
+     */
+    private TreeNode[] listToArray(ListNode list) {
+        int len = listLength(list);
+        ListNode cur = list;
+        TreeNode[] nodes = new TreeNode[len];
+        for (int i = 0; i < len; i++) {
+            nodes[i] = new TreeNode(cur.val);
+            cur = cur.next;
+        }
+        return nodes;
+    }
+
+    private TreeNode sortedListToBST(ListNode list, int start, int end) {
+        if (start > end) return null;
+
+        int mid = start + (end - start) / 2;
+        TreeNode leftChild = sortedListToBST(list, start, mid - 1);
+        TreeNode parent = new TreeNode(nth_Node(list, mid).val);
+        parent.left = leftChild;
+        list = list.next;
+        parent.right = sortedListToBST(list, mid + 1, end);
+
+        return parent;
+    }
+
+    /**
+     * 取得该位置之后的第 n 个节点
+     *
+     * @param list
+     * @param n
+     * @return
+     */
+    private ListNode nth_Node(ListNode list, int n) {
+        ListNode cur = list;
+        while (n > 0) {
+            n--;
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+    /**
+     * 计算链表的长度
+     *
+     * @param list
+     * @return
+     */
+    private int listLength(ListNode list) {
+        if (list == null) return 0;
         int len = 0;
-        ListNode node = head;
-        while (node != null) {
-            node = node.next;
+        ListNode cur = list;
+        while (cur != null) {
+            cur = cur.next;
             len++;
         }
-        Hashtable hashtable;
-        HashMap map;
-        HashSet set;
-        List list;
-        ArrayList list1;
-        return null;
+        return len;
     }
 }
