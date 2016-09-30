7e7461f90a9bf993cd7a89588bbc537d4787fb11
ubuntuck <1196355049@qq.com>
2016-09-30 12:59:39 +0800

    RegularExpressionMatching

--- src/main/java/leetcode/L010_RegularExpressionMatching.java
+++ src/main/java/leetcode/L010_RegularExpressionMatching.java
@@ -24,7 +24,7 @@ package leetcode;
  */
 @Deprecated
 public class L010_RegularExpressionMatching {
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch01(String s, String p) {
         /**
          * This solution is assuming s has no regular expressions.
          *
@@ -73,5 +73,41 @@ public class L010_RegularExpressionMatching {
             }
         }
         return res[n];
+    }
+
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+        int plen = p.length();
+
+        if (slen == 0 && plen == 0) return true;
+
+        char c0 = getChar(s, 0);
+        char p0 = getChar(p, 0), p1 = getChar(p, 1);
+
+        if (match(c0, p0) || p1 == '*') {
+            if (p1 != '*') {
+                if (slen == 0) return false;
+                return isMatch(s.substring(1), p.substring(1));
+            }
+            // if p1 is *, * means 0 ~ n
+            int i = 0;
+            boolean ret = isMatch(s.substring(0), p.substring(2)); // try 0
+            if (ret) return ret;
+            while (i < slen && match(getChar(s, i), p0)) {
+                ret = isMatch(s.substring(i + 1), p.substring(2)); // try for every available position
+                if (ret) return ret;
+                i++;
+            }
+        }
+        return false;
+    }
+
+    private boolean match(char a, char b) {
+        return a == b || b == '.';
+    }
+
+    private char getChar(String s, int p) {
+        if (s.length() > p) return s.charAt(p);
+        return 0;
     }
 }
