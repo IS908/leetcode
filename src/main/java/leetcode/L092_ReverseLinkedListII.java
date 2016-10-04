0f438aae31433ea7a89b55a1502322a7d8fcac09
ubuntuck <1196355049@qq.com>
2016-10-04 14:52:43 +0800

    ReverseLinkedListII

--- src/main/java/leetcode/L092_ReverseLinkedListII.java
+++ src/main/java/leetcode/L092_ReverseLinkedListII.java
@@ -19,8 +19,18 @@ import leetcode.Utils.ListNode;
 @Deprecated // 细节处理，很多边界检查，15分钟做到 bug free 很有难度
 public class L092_ReverseLinkedListII {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode dummy = head;
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode dummy = newHead;
+        for (int i = 1; i < m; i++) dummy = dummy.next;
 
-        return head;
+        ListNode pre = dummy.next, cur = pre.next;
+        for (int j = m; j < n; j++) {   // 头插法实现翻转
+            pre.next = cur.next;
+            cur.next = dummy.next;
+            dummy.next = cur;
+            cur = pre.next;
+        }
+        return newHead.next;
     }
 }
