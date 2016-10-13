3e18e33193d1ce24b1a6e1d7c5c06cffd3bfb838
ubuntuck <1196355049@qq.com>
2016-10-13 20:15:44 +0800

    RomanToInteger

--- src/main/java/leetcode/L013_RomanToInteger.java
+++ src/main/java/leetcode/L013_RomanToInteger.java
@@ -7,14 +7,41 @@ package leetcode;
  * <p/>
  * Created by kevin on 2016/2/29.
  */
-@Deprecated
 public class L013_RomanToInteger {
-	public int romanToInt(String s) {
-		final int[] radix = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-		final String[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-		int result = 0;
+    public int romanToInt(String s) {
+        final int[] radix = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String tmp;
+            int j = 0;
+            boolean flag = false;
+            if (i < s.length() - 1) {
+                tmp = s.substring(i, i + 2);
+                for (; j < roman.length; j++) {
+                    if (roman[j].equals(tmp)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result += radix[j];
+                    i++;
+                    continue;
+                }
+            }
 
-		return 0;
-	}
-
+            tmp = s.substring(i, i + 1);
+            j = 0;
+            flag = false;
+            for (; j < roman.length; j++) {
+                if (roman[j].equals(tmp)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) result += radix[j];
+        }
+        return result;
+    }
 }
