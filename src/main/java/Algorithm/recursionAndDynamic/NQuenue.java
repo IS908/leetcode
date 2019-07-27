ccb6763bbfbd8f1996c49a4cbd5b85e8a9306561
IS908 <kunchen@pku.edu.cn>
2019-07-27 11:11:17 +0800

    N皇后问题

---
+++ src/main/java/Algorithm/recursionAndDynamic/NQuenue.java
@@ -0,0 +1,84 @@
+package Algorithm.recursionAndDynamic;
+
+/**
+ * @Description: N 皇后问题
+ * <p>
+ * N皇后问题是指在N×N的棋盘上要摆N个皇后，要求任何两个皇后不同行、不同列，也不在同一条斜线上。
+ * 给定一个整数n，返回n皇后的摆法有多少种。
+ * @Auther: kun
+ * @Date: 2019-07-27 10:50
+ */
+public class NQuenue {
+
+    /**
+     * 递归方式解决
+     *
+     * @param n
+     * @return
+     */
+    public int num1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        int[] record = new int[n];
+        return process1(0, record, n);
+    }
+
+    public int process1(int i, int[] record, int n) {
+        if (i == n) {
+            return 1;
+        }
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            if (isValid(record, i, j)) {
+                record[i] = j;
+                res += process1(i + 1, record, n);
+            }
+        }
+        return res;
+    }
+
+    public boolean isValid(int[] record, int i, int j) {
+        for (int k = 0; k < i; k++) {
+            if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public int num2(int n) {
+        // 因为本方法中位运算的载体是int型变量，所以该方法只能算1~32皇后问题
+        // 如果想计算更多的皇后问题，需使用包含更多位的变量
+        if (n < 1 || n > 32) {
+            return 0;
+        }
+        int upperLim = n == 32 ? -1 : (1 << n) - 1;
+        return process2(upperLim, 0, 0, 0);
+    }
+
+    public int process2(int upperLim, int colLim, int leftDiaLim, int rightDiaLim) {
+        if (colLim == upperLim) {
+            return 1;
+        }
+        int pos = 0;
+        int mostRightOne = 0;
+        pos = upperLim & (~(colLim | leftDiaLim | rightDiaLim));
+        int res = 0;
+        while (pos != 0) {
+            mostRightOne = pos & (~pos + 1);
+            pos = pos - mostRightOne;
+            res += process2(upperLim,
+                    colLim | mostRightOne,
+                    (leftDiaLim | mostRightOne) << 1,
+                    (rightDiaLim | mostRightOne) >>> 1);
+        }
+        return res;
+    }
+
+}
